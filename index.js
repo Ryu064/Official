289f0192de98aaa4a0be3a3973dee861c3f8da7a
Ryu064 <57674572+Ryu064@users.noreply.github.com>
2020-07-30 23:14:27 +0900

    .
    .

---
+++ index.js
@@ -0,0 +1,15 @@
+const youtubelink = document.getElementById("youtubelink");
+const openIt1 = (() => { open( "https://www.youtube.com/channel/UCMXDsC0noFbk8F1i4vmUpOQ?view_as=subscriber", "_blank"); });
+youtubelink. addEventListener( "click", openIt1, false);
+
+const applelink = document.getElementById("applelink");
+const openIt2 = (() => { open( "https://www.apple.com/kr/apple-music/", "_blank"); });
+applelink. addEventListener( "click", openIt2, false);
+
+const amazonlink = document.getElementById("amazonlink");
+const openIt3 = (() => { open( "https://www.amazon.com/MP3-Music-Download/b?node=163856011", "_blank"); });
+amazonlink. addEventListener( "click", openIt3, false);
+
+const goodslink = document.getElementById("goodslink");
+const openIt4 = (() => { open( "https://sell.smartstore.naver.com/?NaPm=ct%3Dkd8vmw0x%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3Da669ae99302de0b4ca6cb2f4803b8d862bc4ae81#/home/about", "_blank"); });
+goodslink. addEventListener( "click", openIt4, false);
