29729e3bc243c7c2f67de4bda4bf35c66f9e0b24
Ryu064 <57674572+Ryu064@users.noreply.github.com>
2020-08-02 23:40:28 +0900

    .

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const youtubelink = document.getElementById("youtubelink");
-const openIt1 = (() => { open( "https://www.youtube.com/channel/UCMXDsC0noFbk8F1i4vmUpOQ?view_as=subscriber", "_blank"); });
-youtubelink. addEventListener( "click", openIt1, false);
+var youtubelink = document.getElementById("youtubelink");
+youtubelink. addEventListener( "click", youtubeclick, false);
+function youtubeclick(){
+    window.open("https://www.youtube.com/channel/UCMXDsC0noFbk8F1i4vmUpOQ?view_as=subscriber")
+}
 
-const applelink = document.getElementById("applelink");
-const openIt2 = (() => { open( "https://www.apple.com/kr/apple-music/", "_blank"); });
-applelink. addEventListener( "click", openIt2, false);
+var applelink = document.getElementById("applelink");
+applelink. addEventListener( "click", appleclick, false);
+function appleclick(){
+    window.open("https://www.apple.com/kr/apple-music");
+}
 
-const amazonlink = document.getElementById("amazonlink");
-const openIt3 = (() => { open( "https://www.amazon.com/MP3-Music-Download/b?node=163856011", "_blank"); });
-amazonlink. addEventListener( "click", openIt3, false);
+var amazonlink = document.getElementById("amazonlink");
+amazonlink. addEventListener( "click", amazonclick, false);
+function amazonclick(){
+    window.open("https://www.amazon.com/MP3-Music-Download/b?node=163856011");
+}
 
-const goodslink = document.getElementById("goodslink");
-const openIt4 = (() => { open( "https://sell.smartstore.naver.com/?NaPm=ct%3Dkd8vmw0x%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3Da669ae99302de0b4ca6cb2f4803b8d862bc4ae81#/home/about", "_blank"); });
-goodslink. addEventListener( "click", openIt4, false);
+var goodslink = document.getElementById("goodslink");
+goodslink. addEventListener( "click", goodsclick, false);
+function goodsclick(){
+    window.open("https://sell.smartstore.naver.com/?NaPm=ct%3Dkd8vmw0x%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3Da669ae99302de0b4ca6cb2f4803b8d862bc4ae81#/home/about");
+}
+
+var ticketlink = document.getElementById("ticketlink-1");
+ticketlink. addEventListener( "click", ticketclick, false);
+function ticketclick(){
+    window.open("https://www.facebook.com/mmetalcity/")
+}
